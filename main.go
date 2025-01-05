be563c9e3d941a324366933dca7be364101a89a3
Varun <2003varunp@gmail.com>
2025-01-05 12:14:07 +0530

    implement WebSocket server with Gorilla WebSocket and add Makefile for running the application

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func websocketHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+
+	defer conn.Close()
+
+	for {
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("recv: %s", message)
+	}
+
+}
+
+func main() {
+	http.HandleFunc("/ws", websocketHandler)
+	fmt.Println("Server started on :8080")
+	http.ListenAndServe(":8080", nil)
+}
